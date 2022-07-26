4d195c34ecfdf860dcc736c12433b3a001317ad5
yamoo9 <yamoo9@naver.com>
2022-07-26 16:06:22 +0900

    learn. ES(JS) Modules part II

--- src/chapter/09-modules.js
+++ src/chapter/09-modules.js
@@ -1,6 +1,19 @@
 // 1. utils 모듈을 "모듈 객체"로 불러옵니다. (import)
-import * as Utils from '../utils/index.js';
+import * as Utils from '../utils/index.js'; // { numberWithComma, currency, currencyKR }
+// import * as React from 'react';
+
+let price = 908_800_000;
+let result = Utils.currencyKR(price);
+// console.log('DEBUG ~ result', result);
 
 // 2. utils 모듈 중, numberWithComma 함수를 추출합니다.
+import { numberWithComma } from '../utils/index.js';
+// import { defineProps, defineEmits } from 'vue';
 
-// 3. utils 모듈 중, currencyKR 함수를 currency (별칭) 이름으로 추출합니다.
+// console.log(numberWithComma(price));
+
+// 3. utils 모듈 중, currencyKR 함수를 currency (별칭: alias) 이름으로 추출합니다.
+import { currencyKR as currency } from '../utils/index.js';
+// import { createElement as h } from 'react';
+
+// console.log(currency(price));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-// 외부에서 내보낸 모듈을 불러와 사용할 수 있음
-import * as Utils from './utils/index.js';
-// import './chapter/09-modules.js';
-
-let result = Utils.currencyKR(908_800_000);
+// 외부 모듈을 불러와 사용할 수 있음
+import './chapter/09-modules.js';
