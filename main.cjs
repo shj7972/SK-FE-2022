4f1aab4cec0f4face9544ea776a910b129478bf3
yamoo9 <yamoo9@naver.com>
2022-07-26 16:32:50 +0900

    learn. Node.js 런타임 환경에서 ES 모듈 사용 시
    - CommonJS 진영 모듈 *.cjs
    - ES 표준 모듈 *.mjs

--- main.cjs
+++ main.cjs
@@ -4,6 +4,7 @@
 // export default => module.exports
 // export => exports.moduleName
 
-const { numberWithComma } = require('./modules/numberWithComma');
+const { numberWithComma } = require('./modules/numberWithComma.cjs');
+// import { numberWithComma } from './modules/numberWithComma.js';
 
 console.log(numberWithComma(9e6));
