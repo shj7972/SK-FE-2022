9f54d6480cb673e95d117026825e990578c3c5cc
yamoo9 <yamoo9@naver.com>
2022-07-26 14:38:56 +0900

    learn. 전개 구문(spread syntax)
    - 배열 합성 (extend array)
    - 객체 합성 (extend object)

--- src/chapter/04-spread-syntax.js
+++ src/chapter/04-spread-syntax.js
@@ -11,14 +11,16 @@
 const integers = [-1, 0, 32, -101, 24];
 const floatings = [0.98, 10.43, 10.4e5];
 // let maxInt = Math.max.apply(Math, [302, 1, 2, 30, -101].concat(integers));
-let maxInt = Math.max(...[302, ...floatings, 1, ...integers, 2, 30, -101])
+let maxInt = Math.max(...[302, ...floatings, 1, ...integers, 2, 30, -101]);
 
-console.log(maxInt);
+// console.log(maxInt);
 
 /* 객체 전개 ------------------------------------------------------------------- */
 
 // 객체 합성 유틸리티 함수
-var extend = function () {
+// Object.assign 객체 합성 (얕은 복사)
+// extend 유틸리티 객체 합성 (깊은 복사)
+const extend = function () {
   var _mixinObject = arguments[0];
   var _restObjects = [].slice.call(arguments, 1);
 
@@ -46,8 +48,18 @@ var extend = function () {
 };
 
 // 상태 업데이트 유틸리티 함수
-var setState = function(newState) {
-  return extend({}, state, newState);
+const setState = (newState) => {
+  // return extend({}, state, newState);
+
+  // 깊은 복사
+  return {
+    ...state /* { loading: false, error, data } */,
+    ...newState, /* { loading: true, data } */
+    data: [
+      ...state.data,
+      ...newState.data
+    ]
+  }
 };
 
 /* -------------------------------------------------------------------------- */
@@ -55,13 +67,22 @@ var setState = function(newState) {
 // 상태 객체 (불변 데이터 화)
 // React = 선언형 프로그래밍 패러다임
 // 불변(immutable) 데이터 관리
-var state = Object.freeze({
+const state = Object.freeze({
   loading: false,
   error: null,
   data: [{ id: 101, title: '초기 데이터' }],
 });
 
-var updatedState = setState({ 
+// 상태(state) 업데이트 
+// JavaScript 객체는 뮤터블(mutable)
+// Object.freeze()를 사용하면 객체를 불변 객체로 변경 (읽기 전용)
+// state.loading = true;
+// delete state.data;
+
+// console.table(state);
+
+
+const updatedState = setState({ 
   loading: true,
   data: [{ id: 201, title: '데이터 업데이트' }]
 })
