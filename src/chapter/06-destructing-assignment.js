91c30c43f3e715a24f2978cb61a7040232177ca6
yamoo9 <yamoo9@naver.com>
2022-07-26 15:08:32 +0900

    learn. destructuring assignment
    - { 객체 구조 분해 할당 }
    - [ 배열 구조 분해 할당 ]

--- src/chapter/06-destructing-assignment.js
+++ src/chapter/06-destructing-assignment.js
@@ -2,7 +2,7 @@
 /* ECMAScript NEXT를 사용해 함수를 작성합니다.                                       */
 /* -------------------------------------------------------------------------- */
 
-var state = Object.freeze({
+const state = Object.freeze({
   loading: false,
   error: null,
   data: [
@@ -12,11 +12,23 @@ var state = Object.freeze({
   ],
 });
 
-// 객체 속성과 일치하는 변수 선언 후, 속성 값 할당
-var loading = state.loading;
-var error = state.error;
-var data = state.data;
+{
+  // console.table(state);
 
-// 배열의 첫번째 아이템, 나머지 집합 추출
-var firstData = data[0];
-var restData = data.slice(1);
+  // 객체 속성과 일치하는 변수 선언 후, 속성 값 할당
+  // let loading = state.loading;
+  // const error = state.error;
+  // const data = state.data;
+  const { data, loading } = state;
+
+  // console.log('DEBUG ~ loading', loading);
+  // console.log('DEBUG ~ error', error);
+  console.table(data);
+
+  // 배열의 첫번째 아이템, 나머지 집합 추출
+  const [firstData, ...restData] = data;
+  // const firstData = data[0];
+  // const restData = data.slice(1);
+  console.log('DEBUG ~ firstData', firstData);
+  console.log('DEBUG ~ restData', restData);
+}
