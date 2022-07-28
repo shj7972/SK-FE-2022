f657c73d527ee07bc65c6b6522ac2de04429f2a7
yamoo9 <yamoo9@naver.com>
2022-07-28 11:04:49 +0900

    config. learn/jsxInAction.js 추가

--- random-count-up/src/components/index.js
+++ random-count-up/src/components/index.js
@@ -1,4 +1,6 @@
 export { default as Header } from './Header/Header';
+export * from './PrettyPrintCode/PrettyPrintCode';
 export * from './RandomCountUp/RandomCountUp';
+export * from './EmojiOops/EmojiOops';
 export * from './Footer/Footer';
 export * from './Layout/Base';
