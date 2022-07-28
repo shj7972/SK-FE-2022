3dbe7db6a6e37ac01b97b97fc49f00d60bfe6f46
yamoo9 <yamoo9@naver.com>
2022-07-28 14:07:04 +0900

    config. 학습 준비 (이미지 배너 추가)

--- random-count-up/src/pages/SFC.js
+++ random-count-up/src/pages/SFC.js
@@ -1,9 +1,13 @@
+import { getPublicAsset } from 'utils';
+
 function StatelessComponent(props) {
+
   return (
     <div style={{padding: '40px 60px', fontSize: '1.2rem'}}>
-      <h2>
-        stateless function component
-      </h2>
+
+      <a href="#a11y-hidden">[Skip to content Link] Go to A11Y Hidden Component</a>
+
+      <h2> stateless function component </h2>
       <a href="https://reactjs.org">Learn React</a>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cum
@@ -11,12 +15,19 @@ function StatelessComponent(props) {
         repudiandae architecto consequuntur eligendi quibusdam, ducimus corporis
         ipsam animi necessitatibus, quod quis.
       </p>
+
+      <figure className="banner" style={{margin: 0}}>
+        <img src={getPublicAsset('cover.jpg')} alt="" height={400} />
+        <figcaption>선한 영향력, 플라스틱으로부터 바다를 지키는 능력 우리 모두를 생각하는 '선한 영향력'이 더 큰 영향력이 되도록...</figcaption>
+      </figure>
+
       <p>
         Nam officia quis quod, consequatur accusamus earum temporibus quidem
         saepe possimus sunt error laboriosam nesciunt alias laborum doloribus
         est sapiente eveniet rerum asperiores, magnam ratione? Deserunt
         doloremque qui quam velit!
       </p>
+      <h3 id="a11y-hidden"><abbr title="Accessibility">A11Y</abbr> Hidden Component</h3>
       <p>
         Dolorem accusamus aperiam molestias architecto obcaecati earum neque
         ducimus dolore hic, esse ut recusandae fugit quod enim, quae rem sint
