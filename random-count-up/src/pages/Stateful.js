e325ceb6fbfffdfad3b73be35618f9fc8e787a72
yamoo9 <yamoo9@naver.com>
2022-07-29 11:50:58 +0900

    learn. PropTypes.shape, PropTypes.exact 유틸리티 활용

--- random-count-up/src/pages/Stateful.js
+++ random-count-up/src/pages/Stateful.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { InteractHeadline, A11yHidden } from 'components';
-import { arrayOf, oneOfType, number, string } from 'prop-types';
+import { arrayOf, oneOfType, number, string, exact } from 'prop-types';
 
 class StatefulComponent extends Component {
   static defaultProps = {
@@ -19,10 +19,18 @@ class StatefulComponent extends Component {
     containerStyle: {
       padding: 40,
     },
+    members: [
+      { id: 'user-1', name: '해오랑', age: 23 },
+      { id: 'user-2', name: '김이박', age: 45 },
+      { id: 'user-3', name: '박현식', age: 39 },
+      { id: 'user-4', name: '최연지', age: 28 },
+      // new user
+      { id: 'user-5', name: '박사랑', age: 19, gender: 'female' },
+    ],
   };
 
   render() {
-    const { isShowChild, containerStyle, color, background, theme } =
+    const { isShowChild, containerStyle, color, background, theme, members } =
       this.state;
 
     return (
@@ -40,7 +48,9 @@ class StatefulComponent extends Component {
           onToggleChild={this.handleToggleChild}
         />
 
-        {isShowChild && <StatefulComponent.Child numbers={[20, 3034, '22']} />}
+        {isShowChild && (
+          <StatefulComponent.Child numbers={[20, 3034, '22']} users={members} />
+        )}
       </div>
     );
   }
@@ -60,16 +70,23 @@ class StatefulComponent extends Component {
   };
 }
 
+const UserType = exact({
+  id: string.isRequired,
+  name: string.isRequired,
+  age: number.isRequired,
+});
+
 StatefulComponent.Child = class extends Component {
   clearId = null;
 
   static displayName = 'StatefulChild';
   static propTypes = {
     numbers: arrayOf(oneOfType([number, string])),
+    users: arrayOf(UserType),
   };
 
   render() {
-    const { numbers } = this.props;
+    const { numbers, users } = this.props;
     return (
       <>
         <A11yHidden as="h3">숫자 목록</A11yHidden>
@@ -78,6 +95,19 @@ StatefulComponent.Child = class extends Component {
             numbers.map((number, index) => <li key={index}>{number}</li>)
           ) : (
             <li>출력할 숫자가 없습니다.</li>
+          )}
+        </ul>
+        <A11yHidden as="h3">사용자 목록</A11yHidden>
+        <ul>
+          {users && users.length > 0 ? (
+            users.map(({ id, name, age }, index) => (
+              <li key={id}>
+                <span className="user-name">{name}</span> |{' '}
+                <span className="user-age">{age}</span>
+              </li>
+            ))
+          ) : (
+            <li>사용자가 없습니다.</li>
           )}
         </ul>
       </>
