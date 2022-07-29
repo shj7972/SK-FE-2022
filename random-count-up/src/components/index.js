58300229e2b73aeff2c2b16f887828b412e73ab5
yamoo9 <yamoo9@naver.com>
2022-07-29 10:49:25 +0900

    learn. Sass 프리프로세서, Props 기본 값 설정

--- random-count-up/src/components/index.js
+++ random-count-up/src/components/index.js
@@ -4,6 +4,7 @@ export * from './PrettyPrintCode/PrettyPrintCode';
 export * from './A11yHidden/A11yHidden';
 export * from './EmojiOops/EmojiOops';
 export * from './InteractHeadline';
+export * from './Button/Button';
 export * from './Footer/Footer';
 export * from './Banner/Banner';
 export * from './Layout/Base';
